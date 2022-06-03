e9f6b02bbbb6754f86cdf229e130eb286350e32a
scarf <greenscarf005@gmail.com>
2022-06-03 13:13:20 +0900

    feat: install brew to goinfre

--- scarftoolbox/brew/install_brew.py
+++ scarftoolbox/brew/install_brew.py
@@ -1,5 +1,32 @@
+import subprocess
+
 from ..config import *
+
+BREW_PATH = GOINFRE / "brew"
+BREW_BIN = BREW_PATH / "bin"
 
 
 def main():
-    ...
+
+    if BREW_BIN.exists():
+        return
+
+    BREW_PATH.mkdir(parents=True, exist_ok=True)
+    download_cmd = ["curl -L https://github.com/Homebrew/brew/tarball/master"]
+    unzip_command = [f"tar xz --strip 1 -C {BREW_PATH.absolute()}"]
+
+    download_proc = subprocess.run(
+        download_cmd,
+        stdout=subprocess.PIPE,
+        cwd=BREW_PATH,
+        shell=True,
+    )
+    unzip_proc = subprocess.run(
+        unzip_command,
+        input=download_proc.stdout,
+        # stdout=subprocess.PIPE,
+        cwd=BREW_PATH,
+        shell=True,
+    )
+    subprocess.run([f"{BREW_BIN / 'brew'}"])
+    print(f"brew installed, add {BREW_BIN} to PATH`")
