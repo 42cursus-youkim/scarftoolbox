12d6ab8cb85944b94486250e0fa74eac82977e59
scarf <greenscarf005@gmail.com>
2022-06-03 09:51:57 +0900

    fix: move files better

--- cache/symlink_cache.py
+++ cache/symlink_cache.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
+
 """
-Copyright 2022 scarf
+How To Use
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+python3 symlink_cache.py
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+configure NAMES for whatever you feel belongs in goinfre.
 """
-
 
 from __future__ import annotations
 
+import shutil
 from os import environ, system
 from pathlib import Path
-from shutil import move
 from typing import Dict, Generator, List
 
 """
 Constants
 """
-CacheDirs = Dict[Path, List[str]]  # Type alias
+CacheDirs = Dict[Path, List[str]]
 HOME = Path(environ["HOME"])
 app_support = HOME / "Library" / "Application Support"
 goinfre = HOME / "goinfre" / "Cache"
@@ -63,8 +61,6 @@ NAMES: CacheDirs = {
     / "Google"
     / "Chrome"
     / "Profile 5": ["Service Worker/CacheStorage"],
-    HOME / ".brew" / "Library" / "Taps" / "homebrew": ["homebrew-core/.git"],
-    HOME / ".brew": [".git"],
     HOME / ".vscode": ["extensions"],
 }
 
@@ -82,43 +78,32 @@ def to_goinfre(path: Path) -> Path:
     return goinfre / path.relative_to(HOME)
 
 
-def move_to_goinfre(file: Path, names: CacheDirs = NAMES):
-    create_goinfre_caches(names)
+def move_dir_to_goinfre(file: Path) -> None:
+    def move_recursively(link: Path, goinfre: Path) -> None:
+        shutil.copytree(link, goinfre, dirs_exist_ok=True)
+        shutil.rmtree(link)
+        print(f"{link} -> {goinfre}", end=" ")
+
     link, goinfre = Path(file), to_goinfre(file)
-    goinfre.mkdir(parents=True, exist_ok=True)
-    try:
-        if link.is_symlink() or link.is_file():
-            link.unlink()
-        if link.is_dir():
-            move(str(link), goinfre)
-            print(f"{link} -> {goinfre}")
+    if not goinfre.is_dir():
+        goinfre.unlink()
+        goinfre.mkdir(parents=True)
+    if link.is_symlink():
+        return
+    if link.is_file():
+        link.unlink()
+    if link.is_dir():  # Move them
+        move_recursively(link, goinfre)
+    if not link.exists():
+        print(f"created link", end="")
         link.symlink_to(goinfre)
-    except Exception as e:
-        print(f"skipped due to {e}")
-
-
-def move_all_cache_to_goinfre(names: CacheDirs):
-    for f in caches(names):
-        move_to_goinfre(f)
-
-
-def create_goinfre_caches(names: CacheDirs):
-    for f in caches(names):
-        to_goinfre(f).mkdir(parents=True, exist_ok=True)
+    print()
 
 
 def main():
-    # move_all_cache_to_goinfre(NAMES)
-    create_goinfre_caches(NAMES)
+    for f in caches(NAMES):
+        move_dir_to_goinfre(f)
 
 
 if __name__ == "__main__":
     main()
-
-"""
-How To Use
-
-python3 symlink_cache.py
-
-configure NAMES for whatever you feel belongs in goinfre.
-"""
--- utils.sh
+++ utils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 $SCARFUTILS/vscode/install_vscode.py
+python3 $SCARFTOOL/vscode/install_vscode.py
 export PATH="$PATH:$HOME/goinfre/vscode/Visual Studio Code.app/Contents/Resources/app/bin"
