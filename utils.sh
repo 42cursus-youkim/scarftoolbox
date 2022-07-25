27bb64cbea04f3c455700f8dc4e1b7e883376705
scarf <greenscarf005@gmail.com>
2022-07-25 16:38:27 +0900

    feat: save pip and brew status

--- utils.sh
+++ utils.sh
@@ -2,3 +2,25 @@
 
 python3 $SCARFTOOL/vscode/install_vscode.py
 export PATH="$PATH:$HOME/goinfre/vscode/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# python
+py_dep () {
+  echo installing py deps
+  local PY=$(which python3)
+  local REQ=~/.pyreq.txt
+  [[ -f $REQ ]] && $PY -m pip install -r $REQ
+  mv $REQ $REQ.old
+  $PY -m pip freeze > $REQ
+}
+
+# brew
+brew_dep() {
+  echo installing brew deps
+  local REQ=~/.Brewfile
+  [[ -f $REQ ]] && brew bundle --file=$REQ
+  mv $REQ $REQ.old
+  brew bundle dump --file=$REQ --force
+}
+
+brew_dep
+py_dep
